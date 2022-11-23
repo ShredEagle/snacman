3d5817f5b9601e9b8a998097bbce9cb2568c60d8
Adrien <Adnn@users.noreply.github.com>
2022-11-23 18:25:40 +0100

    Add a third cube, which is added and removed every other second.

--- src/apps/snacman/snacman/simulations/cubes/Cubes.cpp
+++ src/apps/snacman/snacman/simulations/cubes/Cubes.cpp
@@ -13,7 +13,8 @@ Cubes::Cubes(graphics::AppInterface & aAppInterface) :
     mAppInterface{&aAppInterface},
     mSystemMove{mWorld.addEntity()},
     mSystemOrbitalCamera{mWorld, mWorld},
-    mQueryRenderable{mWorld, mWorld}
+    mQueryRenderable{mWorld, mWorld},
+    mBlinkingCube{mWorld.addEntity()}
 {
     ent::Phase init;
     mSystemMove.get(init)->add(system::Move{mWorld});
@@ -33,13 +34,35 @@ Cubes::Cubes(graphics::AppInterface & aAppInterface) :
 
 void Cubes::update(float aDelta, const snac::Input & aInput)
 {
+    mSimulationTime += aDelta; 
+
     ent::Phase update;
+
+    blinkCube(update);
+
     mSystemMove.get(update)->get<system::Move>().update(aDelta);
     mSystemOrbitalCamera->update(aInput,
                                  getCameraParameters().vFov,
                                  mAppInterface->getWindowSize().height());
 }
 
+void Cubes::blinkCube(ent::Phase & aPhase)
+{
+    auto cube = mBlinkingCube.get(aPhase);
+    if((int)mSimulationTime % 2 == 0 && cube)
+    {
+        cube->erase();
+    }
+    else if((int)mSimulationTime % 2 == 1 && !cube)
+    {
+        mBlinkingCube = mWorld.addEntity();
+        mBlinkingCube.get(aPhase)->add(component::Geometry{
+            .mPosition = { 0.f, 1.f, -3.f},
+            .mYRotation = math::Degree<float>{45.f},
+        });
+    }
+
+}
 
 std::unique_ptr<visu::GraphicState> Cubes::makeGraphicState() const
 {
