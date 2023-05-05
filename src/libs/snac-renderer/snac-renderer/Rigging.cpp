64a99593572dc226b209be7fbfc688f8d145a108
Adrien <Adnn@users.noreply.github.com>
2023-05-05 15:15:24 +0200

    Interpolate between the keyframes' poses.

--- src/libs/snac-renderer/snac-renderer/Rigging.cpp
+++ src/libs/snac-renderer/snac-renderer/Rigging.cpp
@@ -1,6 +1,8 @@
 #include "Rigging.h"
 
 #include <math/Transformations.h>
+#include <math/Interpolation/Interpolation.h>
+#include <math/Interpolation/QuaternionInterpolation.h>
 
 
 namespace ad {
@@ -18,30 +20,78 @@ namespace {
                 * math::trans3d::translate(aKeyframes.mTranslations[aStepIndex]);
     }
 
+
+    math::AffineMatrix<4, float> computeMatrix(
+        const NodeAnimation::NodeKeyframes & aKeyframes,
+        std::size_t aPreviousStep, std::size_t aNextStep,
+        float aInterpolant)
+    {
+        return 
+            math::trans3d::scale(
+                math::lerp(aKeyframes.mScales[aPreviousStep].as<math::Size>(),
+                           aKeyframes.mScales[aNextStep].as<math::Size>(),
+                           aInterpolant))
+            * math::slerp(aKeyframes.mRotations[aPreviousStep],
+                          aKeyframes.mRotations[aNextStep],
+                          aInterpolant).toRotationMatrix()
+            * math::trans3d::translate(
+                math::lerp(aKeyframes.mTranslations[aPreviousStep],
+                           aKeyframes.mTranslations[aNextStep],
+                           aInterpolant))
+            ;
+    }
+
 } // namespace unnamed
 
 
 void NodeAnimation::animate(float aTimepoint, NodeTree<math::AffineMatrix<4, float>> & aNodeTree) const
 {
-    // Find the current step and interpolation
-    auto stepAfter =
+    assert(mTimepoints.size() > 0);
+
+    //
+    // Find the indices of the timepoints around the current time,
+    // and the interpolation value.
+    //
+    auto timepointAfter =
         std::find_if(mTimepoints.begin(), mTimepoints.end(),
                      [aTimepoint](float aTime){return aTime > aTimepoint;});
     
-    std::size_t stepIdx = stepAfter != mTimepoints.end() ?
-        stepAfter - mTimepoints.begin()
-        : mTimepoints.size() - 1;
+    std::size_t previousIdx, nextIdx;
+    float interpolant = 0.f;
+    if (timepointAfter == mTimepoints.begin())
+    {
+        previousIdx = nextIdx = 0;
+    }
+    else if(timepointAfter == mTimepoints.end())
+    {
+        previousIdx = nextIdx = mTimepoints.size() - 1;
+    }
+    else
+    {
+        nextIdx = timepointAfter - mTimepoints.begin();
+        previousIdx = nextIdx - 1;
+        interpolant = (aTimepoint - mTimepoints[previousIdx]) 
+                       / (*timepointAfter - mTimepoints[previousIdx]);
+    }
 
-    // The joint index is the index in the NodeAnimation list of joints.
+    //
+    // Compute the new local pose of each animated node
+    //
+
+    // Note: The joint index is the index in the NodeAnimation list of joints.
     // This will be different from the node index in the NodeTree hierarchy.
     for(std::size_t jointIdx = 0; jointIdx != mNodes.size(); ++jointIdx)
     {
         Node::Index hierarchyIdx = mNodes[jointIdx];
         NodeAnimation::NodeKeyframes keyframes = mKeyframesEachNode[jointIdx];
 
-        aNodeTree.mLocalPose[hierarchyIdx] = computeMatrix(mKeyframesEachNode[jointIdx], stepIdx);
+        aNodeTree.mLocalPose[hierarchyIdx] = 
+            computeMatrix(mKeyframesEachNode[jointIdx], previousIdx, nextIdx, interpolant);
     }
 
+    //
+    // Traverse the node tree hierarchy to compute the global pose of each node
+    //
     for(Node::Index nodeIdx = 0; nodeIdx != aNodeTree.mHierarchy.size(); ++nodeIdx)
     {
         if(Node::Index parentIdx = aNodeTree.mHierarchy[nodeIdx].mParent;
