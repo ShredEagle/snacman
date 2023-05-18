fcd736bb19f62d2d8d8f6c074a70b35d96bcc087
Adrien <Adnn@users.noreply.github.com>
2023-05-18 12:35:57 +0200

    Change animations speed: slow idle, hasten run.

--- src/apps/snacman/snacman/simulations/snacgame/system/AnimationManager.cpp
+++ src/apps/snacman/snacman/simulations/snacgame/system/AnimationManager.cpp
@@ -14,12 +14,14 @@ void AnimationManager::update()
 {
     mAnimated.each([](component::PlayerModel & aModel, const component::PlayerMoveState & aMoveState) {
         Phase animPhase;
+        float animSpeed = 0.6f;
         std::string newAnimName = "idle";
         if (aMoveState.mMoveState
             & (gPlayerMoveFlagUp | gPlayerMoveFlagDown | gPlayerMoveFlagRight
                | gPlayerMoveFlagLeft))
         {
             newAnimName = "run";
+            animSpeed = 1.3f;
         }
 
         component::RigAnimation & playerAnim = aModel.mModel.get(animPhase)->get<component::RigAnimation>();
@@ -29,7 +31,10 @@ void AnimationManager::update()
             playerAnim.mAnimation = &playerAnim.mAnimationMap->at(newAnimName);
             playerAnim.mAnimName = newAnimName;
             playerAnim.mStartTime = snac::Clock::now();
-            playerAnim.mParameter = decltype(component::RigAnimation::mParameter){playerAnim.mAnimation->mEndTime};
+            playerAnim.mParameter = decltype(component::RigAnimation::mParameter){
+                playerAnim.mAnimation->mEndTime,
+                animSpeed
+            };
         }
     });
 }
