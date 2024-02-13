2a27bc76079ee35f56ae2c1e18913bc036d1d535
Adrien <Adnn@users.noreply.github.com>
2024-02-13 15:39:52 +0100

    Make a few notes regarding Cube.h.

--- src/libs/snac-renderer-V2/snac-renderer-V2/Cube.h
+++ src/libs/snac-renderer-V2/snac-renderer-V2/Cube.h
@@ -9,10 +9,14 @@
 #include <span>
 
 
-// TODO find a way to factorize that and to move it up in a library
+// TODO Ad 2024/02/13: Find a way to factorize that and to move it up in a library
+// The main design complication is that we want to achieve two "contradictory" goals:
+// * Having high level functions so client code can very easily get a cube, or any basic shape.
+// * Having generic code that does not force the client code to use any predefined memory layout.
 
 namespace ad::renderer {
 
+// WARNING: this is the intial insipiration, but we did re-organize the vertices in our actual code. 
 //constexpr math::Vec<3, float> vertices[8] =
 //{
 //    {-1., -1., -1.},
@@ -267,200 +271,6 @@ inline std::vector<PositionNormal> getExpandedVertices(math::Box<float> aBox)
 //        GL_STATIC_DRAW);
 //    return vbo;
 //}
-//
-//
-//inline VertexStream makeBox(math::Box<float> aBox)
-//{
-//    VertexStream geometry{
-//        .mPrimitive = GL_TRIANGLES,
-//        .mBoundingBox = math::Box<GLfloat>{ {-1.f, -1.f, -1.f}, {2.f, 2.f, 2.f}}
-//    };
-//
-//    // Note: Binding the VAO is not required for loading vertex buffers
-//    // Make a VBO and load the cube vertices into it
-//    // Save the index of the next VBO.
-//    auto index = geometry.mVertexBuffers.size();
-//    auto vertices = getExpandedVertices(aBox);
-//    geometry.mVertexBuffers.push_back({
-//        .mBuffer = loadVBO(std::span{vertices}),
-//        .mStride = sizeof(PositionNormal)
-//    });
-//
-//    {
-//        graphics::ClientAttribute position{
-//            .mDimension = 3,
-//            .mOffset = offsetof(PositionNormal, position),
-//            .mComponentType = GL_FLOAT
-//        };
-//        geometry.mAttributes.emplace(Semantic::Position, AttributeAccessor{index, position});
-//    }
-//    {
-//        graphics::ClientAttribute normal{
-//            .mDimension = 3,
-//            .mOffset = offsetof(PositionNormal, normal),
-//            .mComponentType = GL_FLOAT
-//        };
-//        geometry.mAttributes.emplace(Semantic::Normal, AttributeAccessor{index, normal});
-//    }
-//    geometry.mVertexCount = static_cast<GLsizei>(vertices.size());
-//
-//    return geometry;
-//}
-//
-///// \deprecated
-//inline VertexStream makeCube()
-//{
-//    return makeBox({{-1.f, -1.f, -1.f}, {2.f, 2.f, 2.f}});
-//}
-//
-//
-//inline VertexStream makeArrow()
-//{
-//    VertexStream geometry{
-//        .mPrimitive = GL_LINES,
-//        .mBoundingBox = math::Box<GLfloat>{ {-.15f, 0.f, -0.15f}, {0.3f, 1.f, 0.3f}}
-//    };
-//
-//    auto index = geometry.mVertexBuffers.size();
-//    const auto & vertices = getArrowVertices();
-//    geometry.mVertexBuffers.push_back({
-//        .mBuffer = loadVBO(std::span{vertices}),
-//        .mStride = sizeof(std::remove_cvref_t<decltype(vertices)>::value_type),
-//    });
-//
-//    {
-//        graphics::ClientAttribute position{
-//            .mDimension = 3,
-//            .mOffset = 0,
-//            .mComponentType = GL_FLOAT
-//        };
-//        geometry.mAttributes.emplace(Semantic::Position, AttributeAccessor{index, position});
-//    }
-//    
-//    geometry.mVertexCount = static_cast<GLsizei>(vertices.size());
-//
-//    return geometry;
-//}
-//
-//
-//struct PositionNormalUV
-//{
-//    math::Position<3, float> position;
-//    math::Vec<3, float> normal;
-//    math::Position<2, float> uv;
-//};
-//
-//
-//inline VertexStream makeRectangle(math::Rectangle<GLfloat> aRectangle)
-//{
-//    VertexStream geometry{
-//        .mPrimitive = GL_TRIANGLE_STRIP,
-//    };
-//    // Make a VBO and load the vertices into it
-//    auto index = geometry.mVertexBuffers.size();
-//
-//    std::array<PositionNormalUV, 4> vertices = {
-//        PositionNormalUV{
-//            .position = {aRectangle.topLeft(), 0.f},
-//            .normal = {0.f, 0.f, 1.f},
-//            .uv = {0.f, 1.f}
-//        },
-//        PositionNormalUV{
-//            .position = {aRectangle.bottomLeft(), 0.f},
-//            .normal = {0.f, 0.f, 1.f},
-//            .uv = {0.f, 0.f}
-//        },
-//        PositionNormalUV{
-//            .position = {aRectangle.topRight(), 0.f},
-//            .normal = {0.f, 0.f, 1.f},
-//            .uv = {1.f, 1.f}
-//        },
-//        PositionNormalUV{
-//            .position = {aRectangle.bottomRight(), 0.f},
-//            .normal = {0.f, 0.f, 1.f},
-//            .uv = {1.f, 0.f}
-//        },
-//    };
-//
-//    geometry.mVertexBuffers.push_back({
-//        .mBuffer = loadVBO(std::span{vertices}),
-//        .mStride = sizeof(PositionNormalUV),
-//    });
-//
-//    {
-//        graphics::ClientAttribute position{
-//            .mDimension = 3,
-//            .mOffset = 0,
-//            .mComponentType = GL_FLOAT
-//        };
-//        geometry.mAttributes.emplace(Semantic::Position, AttributeAccessor{index, position});
-//    }
-//    {
-//        graphics::ClientAttribute normal{
-//            .mDimension = 3,
-//            .mOffset = offsetof(PositionNormalUV, normal),
-//            .mComponentType = GL_FLOAT
-//        };
-//        geometry.mAttributes.emplace(Semantic::Normal, AttributeAccessor{index, normal});
-//    }
-//    {
-//        graphics::ClientAttribute uv{
-//            .mDimension = 2,
-//            .mOffset = offsetof(PositionNormalUV, uv),
-//            .mComponentType = GL_FLOAT
-//        };
-//        geometry.mAttributes.emplace(Semantic::TextureCoords0, AttributeAccessor{index, uv});
-//    }
-//
-//    geometry.mVertexCount = static_cast<GLsizei>(vertices.size());
-//
-//    return geometry;
-//}
-//
-//inline VertexStream makeQuad()
-//{
-//    return makeRectangle({{-1.f, -1.f}, {2.f, 2.f}});
-//}
-//
-//inline VertexStream makeTriangle()
-//{
-//    VertexStream geometry{
-//        .mPrimitive = GL_TRIANGLES,
-//    };
-//    // Make a VBO and load the triangle vertices into it
-//    auto index = geometry.mVertexBuffers.size();
-//    std::array<math::Vec<3, float>, 3> vertices = {
-//        math::Vec<3, float>{-0.6f, -0.4f, 0.f},
-//        math::Vec<3, float>{ 0.6f, -0.4f, 0.f},
-//        math::Vec<3, float>{  0.f,  0.6f, 0.f}
-//    };
-//
-//    geometry.mVertexBuffers.push_back({
-//        .mBuffer = loadVBO(std::span{vertices}),
-//    });
-//    graphics::ClientAttribute attribute{
-//        .mDimension = 3,
-//        .mOffset = 0,
-//        .mComponentType = GL_FLOAT
-//    };
-//    geometry.mAttributes.emplace(Semantic::Position, AttributeAccessor{index, attribute});
-//    geometry.mVertexCount = static_cast<GLsizei>(vertices.size());
-//
-//    return geometry;
-//}
 
-
-//float textureBuffer[12 * 6];
-//for (int i = 0; i < 36; i++) {
-//    textureBuffer[i * 2 + 0] = texCoords[texInds[i % 4]].x;
-//    textureBuffer[i * 2 + 1] = texCoords[texInds[i % 4]].y;
-//}
-//
-//float normalBuffer[18 * 6];
-//for (int i = 0; i < 36; i++) {
-//    normalBuffer[i * 3 + 0] = normals[indices[i / 6]].x;
-//    normalBuffer[i * 3 + 1] = normals[indices[i / 6]].y;
-//    normalBuffer[i * 3 + 2] = normals[indices[i / 6]].z;
-//}
 
 } // namespace ad::renderer
