a0490b18e27f6eacb7928248ec75bd90bac91695
Adrien <Adnn@users.noreply.github.com>
2024-07-16 17:38:41 +0200

    Implement correct filtering of the integrated environment BRDF.

--- src/apps/viewer/viewer/EnvironmentUtilities.cpp
+++ src/apps/viewer/viewer/EnvironmentUtilities.cpp
@@ -246,6 +246,15 @@ Handle<graphics::Texture> integrateEnvironmentBrdf(Storage & aStorage,
     // Draw the fullscreen quad (which will invoke the FS for each ouptut pixel of the viewport)
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
+    {
+        graphics::ScopedBind boundTexture{result};
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    }
+
     return &aStorage.mTextures.emplace_back(std::move(result));
 }
 
