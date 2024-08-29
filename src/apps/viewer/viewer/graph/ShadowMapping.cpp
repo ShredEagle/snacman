23bc7ddf3e6afb7a6a86a16b8c81bf13d00517ab
Adrien <Adnn@users.noreply.github.com>
2024-08-29 12:48:48 +0200

    Start working on a dynamic frustum for shadow camera.

--- src/apps/viewer/viewer/graph/ShadowMapping.cpp
+++ src/apps/viewer/viewer/graph/ShadowMapping.cpp
@@ -13,11 +13,33 @@
 namespace ad::renderer {
 
 
+math::Rectangle<GLfloat> getFrustumSideBounds(math::Matrix<4, 4, GLfloat> aFrustumClipToLightSpace)
+{
+    using math::homogeneous::normalize;
+
+    math::Position<2, GLfloat> low = normalize(gNdcUnitCorners[0] * aFrustumClipToLightSpace).xy();
+    math::Position<2, GLfloat> high = low;
+
+    for(std::size_t idx = 1; idx != gNdcUnitCorners.size(); ++idx)
+    {
+        math::Position<2, GLfloat> cornerInLightSpace = normalize(gNdcUnitCorners[idx] * aFrustumClipToLightSpace).xy();
+        low = min(low, cornerInLightSpace);
+        high = max(high, cornerInLightSpace);
+    }
+
+    return math::Rectangle<GLfloat>{
+        .mPosition = low,
+        .mDimension = (high - low).as<math::Size>(),
+    };
+}
+
+
 LightViewProjection fillShadowMap(const ShadowMapping & aPass, 
                                   const RepositoryTexture & aTextureRepository,
                                   Storage & aStorage,
                                   const TheGraph & aGraph,
                                   const ViewerPartList & aPartList,
+                                  math::Matrix<4, 4, GLfloat> aViewProjectionInverse,
                                   Handle<const graphics::Texture> aShadowMap,
                                   std::span<const DirectionalLight> aDirectionalLights,
                                   bool aDebugDrawFrusta)
@@ -55,15 +77,34 @@ LightViewProjection fillShadowMap(const ShadowMapping & aPass,
 
         const DirectionalLight light = aDirectionalLights[directionalIdx];
         Camera lightViewpoint;
+
         // TODO handle lights for which the default up direction does not work (because it is the light gaze direction)
         lightViewpoint.setPose(graphics::getCameraTransform(
             -5 * light.mDirection.as<math::Position>(),
             light.mDirection
         ));
-        // TODO handle the size of the orthographic projection
+
+        //
+        // Handle the orthographic projection.
+        // The bounding box will determinate the size of the projection, and the position of the viewpoint.
+        //
+        
+        // Compute the matrix transforming from the camera view clip space to the light space.
+        const math::Rectangle<GLfloat> lightFrustumSides = getFrustumSideBounds(aViewProjectionInverse * lightViewpoint.getParentToCamera());
+
+        if(aDebugDrawFrusta) 
+        {
+            DBGDRAW_INFO(drawer::gLight).addPlane(
+                  {0.f, 0.f, 0.f},
+                  (math::Vec<4, float>{1.f, 0.f, 0.f, 0.f} * lightViewpoint.getParentToCamera().inverse()).xyz(), 
+                  (math::Vec<4, float>{0.f, 1.f, 0.f, 0.f} * lightViewpoint.getParentToCamera().inverse()).xyz(),
+                  5, 5,
+                  lightFrustumSides.width(), lightFrustumSides.height());
+        }
+
         lightViewpoint.setupOrthographicProjection({
-            .mAspectRatio = 1.f,
-            .mViewHeight = 6.f,
+            .mAspectRatio = math::getRatio<GLfloat>(lightFrustumSides.mDimension),
+            .mViewHeight = lightFrustumSides.mDimension.height(),
             .mNearZ = 0.f,
             .mFarZ = -10.f,
         });
