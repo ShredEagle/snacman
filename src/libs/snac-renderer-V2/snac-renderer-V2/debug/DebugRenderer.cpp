c9c1698b04352d2401e5d2178b3c03a0ac1b22a9
Adrien <Adnn@users.noreply.github.com>
2024-11-06 15:50:59 +0100

    Separate programs from shaders in distinct folders.

--- src/libs/snac-renderer-V2/snac-renderer-V2/debug/DebugRenderer.cpp
+++ src/libs/snac-renderer-V2/snac-renderer-V2/debug/DebugRenderer.cpp
@@ -197,9 +197,9 @@ DebugRenderer::DebugRenderer(Storage & aStorage, const Loader & aLoader) :
                                            mLineVertexBuffer),
         .mPrimitiveMode = GL_LINES,
     },
-    mLineProgram{storeConfiguredProgram(aLoader.loadProgram("shaders/DebugDraw.prog"), aStorage)},
+    mLineProgram{storeConfiguredProgram(aLoader.loadProgram("programs/DebugDraw.prog"), aStorage)},
     mEntryInstanceBuffer{makeBuffer(0, 0, GL_STREAM_DRAW, aStorage)},
-    mEntryProgram{storeConfiguredProgram(aLoader.loadProgram("shaders/DebugDrawModelTransform.prog"), aStorage)},
+    mEntryProgram{storeConfiguredProgram(aLoader.loadProgram("programs/DebugDrawModelTransform.prog"), aStorage)},
     mBox{setupInstancedShape<BoxTag>("DebugBox", aStorage, mEntryInstanceBuffer)},
     mArrow{setupInstancedShape<arrow::Maker>("DebugArrow", aStorage, mEntryInstanceBuffer)}
 {
