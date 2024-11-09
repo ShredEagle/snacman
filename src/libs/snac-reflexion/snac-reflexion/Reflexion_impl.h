48e3a62b054b31e58dbc3bc5555e96206fb945cc
Francois Poizat <francois.poizat@gmail.com>
2024-11-09 12:32:32 +0100

    Starting to allow for non default constructible type erased serialization

--- src/libs/snac-reflexion/snac-reflexion/Reflexion_impl.h
+++ src/libs/snac-reflexion/snac-reflexion/Reflexion_impl.h
@@ -5,6 +5,7 @@
 #include <entity/Entity.h>
 #include <entity/EntityManager.h>
 #include <nlohmann/json.hpp>
+#include <type_traits>
 
 namespace reflexion {
 
@@ -13,12 +14,20 @@ using json = nlohmann::json;
 template<class T_type>
 void TypedProcessor<T_type>::addComponentToHandle(const ad::serial::Witness && aWitness, ad::ent::Handle<ad::ent::Entity> aHandle)
 {
+    if constexpr (std::is_default_constructible_v<T_type>)
+    {
+        {
+            ad::ent::Phase phase;
+            T_type t;
+            aHandle.get(phase)->add(t);
+        }
+        aHandle.get()->get<T_type>().describeTo(aWitness);
+    }
+    else
     {
         ad::ent::Phase phase;
-        T_type t;
-        aHandle.get(phase)->add(t);
+        aHandle.get(phase)->add(T_type::construct(aWitness));
     }
-    aHandle.get()->get<T_type>().describeTo(aWitness);
 }
 
 template<class T_type>
