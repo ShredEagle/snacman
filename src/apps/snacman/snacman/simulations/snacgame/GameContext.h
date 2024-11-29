c9ccdeb2185d3de138f9bb2a8cdc704f6af76633
Francois Poizat <francois.poizat@gmail.com>
2024-11-29 15:21:36 +0100

    fix win again

--- src/apps/snacman/snacman/simulations/snacgame/GameContext.h
+++ src/apps/snacman/snacman/simulations/snacgame/GameContext.h
@@ -50,7 +50,7 @@ inline uint32_t snac_random(pcg32_random_t* rng)
     // Advance internal state
     rng->state = oldstate * 6364136223846793005ULL + (rng->inc|1);
     // Calculate output function (XSH RR), uses old state for max ILP
-    uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+    uint32_t xorshifted = (uint32_t)((oldstate >> 18u) ^ oldstate) >> 27u;
     uint32_t rot = oldstate >> 59u;
     return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
 }
